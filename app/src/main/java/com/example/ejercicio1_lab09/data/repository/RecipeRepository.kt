56044e501cdf9f2141e97e2986b4ffa01b37e150
neB <benjamin.sullca1103@gmail.com>
2025-10-18 14:35:17 -0500

    feat(repository): agregar RecipeRepository

---
+++ app/src/main/java/com/example/ejercicio1_lab09/data/repository/RecipeRepository.kt
@@ -0,0 +1,26 @@
+package com.example.ejercicio1_lab09.data.repository
+
+import com.example.ejercicio1_lab09.data.model.RecipeModel
+import com.example.ejercicio1_lab09.data.network.RecipeApiService
+
+class RecipeRepository(private val api: RecipeApiService) {
+
+    suspend fun getRecipes(limit: Int = 20, skip: Int = 0) =
+        api.getRecipes(limit, skip)
+
+    suspend fun getRecipeById(id: Int) = api.getRecipeById(id)
+
+    suspend fun searchRecipes(q: String) = api.searchRecipes(q)
+
+    suspend fun getTags() = api.getAllTags()
+
+    suspend fun getByTag(tag: String) = api.getRecipesByTag(tag)
+
+    suspend fun getByMeal(meal: String) = api.getRecipesByMeal(meal)
+
+    suspend fun addRecipe(recipe: RecipeModel) = api.addRecipe(recipe)
+
+    suspend fun updateRecipe(id: Int, recipe: RecipeModel) = api.updateRecipe(id, recipe)
+
+    suspend fun deleteRecipe(id: Int) = api.deleteRecipe(id)
+}
